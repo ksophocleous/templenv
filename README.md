98bfe30edaaf117676ff7a7e727e7179efaade7c
Konstantinos Sofokleous <ksophocleous@gmail.com>
2016-10-09 22:14:22 +0200

    initial commit plus README

---
+++ README.md
@@ -0,0 +1,31 @@
+# templenv
+templenv reads golang template files and outputs the result to either stdout or
+to a file (if `-outfile <filename>` is provided).
+
+there are some (only two atm) useful functions you can use:
+- `env "<NAME>"`: returns the value of the environment variable with the name <NAME>
+- `exec "<CMD>"`: executes the cmd in a bash shell retrieves the stdout and strips the final EOL character
+
+# examples
+
+test.templ
+```
+there's no place like home => {{ env "HOME" }}.
+today is {{ exec "cal | head -n 1" }}
+```
+
+`> templenv test.templ`
+```
+there's no place like home => /Users/ksophocleous.
+today is     October 2016
+```
+
+# why?
+I needed an easy way to generate config files from env variables and command outputs, searched for something small and not bash based and found nothing, so I created this.
+
+essentially I wanted to generate ansible inventories that contain ip addresses from terraform outputs, like so
+
+```
+[db]
+{{ exec "terraform output db.node1.ipv4.public" }} var1=somevalue
+```
---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+var outfile = flag.String("outfile", "-", "path to output filename")
+
+func toStrings(args []interface{}) []string {
+	result := []string{}
+	for _, v := range args {
+		result = append(result, v.(string))
+	}
+	return result
+}
+
+func getEnvVariable(args ...interface{}) string {
+	r := []string{}
+	for _, s := range toStrings(args) {
+		r = append(r, os.Getenv(s))
+	}
+	return strings.Join(r, " ")
+}
+
+func execCommand(args ...interface{}) string {
+	arguments := toStrings(args)
+	if len(arguments) <= 0 {
+		fmt.Fprintf(os.Stderr, "ERROR: exec found but no command was given")
+		os.Exit(1)
+	}
+	totalCmd := strings.Join(arguments, " ")
+	cmd := exec.Command("bash", "-c", totalCmd)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: cmd '%s' failed to execute", totalCmd)
+		os.Exit(1)
+	}
+	return stripEol(out.String())
+}
+
+func stripEol(s string) string {
+	s1 := strings.TrimSuffix(s, "\r\n")
+	if s1 != s {
+		return s1
+	}
+	return strings.TrimSuffix(s1, "\n")
+}
+
+func main() {
+	var err error
+
+	flag.Parse()
+
+	if len(flag.Args()) <= 0 {
+		fmt.Fprintf(os.Stderr, "ERROR: must supply the template file\n")
+		os.Exit(1)
+	}
+	infile := flag.Args()[0]
+
+	funcMap := template.FuncMap{
+		"env":  getEnvVariable,
+		"exec": execCommand,
+	}
+
+	of := os.Stdout
+	if *outfile != "-" {
+		of, err = os.Create(*outfile)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "ERROR: Failed to open output file '%s' because '%s'\n", *outfile, err.Error())
+		}
+		defer of.Close()
+	}
+
+	content, err := ioutil.ReadFile(infile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: Failed to read file '%s' because '%s'\n", infile, err.Error())
+		os.Exit(3)
+	}
+
+	templ, err := template.New("Error").Funcs(funcMap).Parse(string(content[:]))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: Failed to parse the template file '%s' because '%s'\n", infile, err.Error())
+		os.Exit(2)
+	}
+
+	err = templ.Execute(of, struct{}{})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: Template execution failed '%s'\n", err.Error())
+	}
+}
